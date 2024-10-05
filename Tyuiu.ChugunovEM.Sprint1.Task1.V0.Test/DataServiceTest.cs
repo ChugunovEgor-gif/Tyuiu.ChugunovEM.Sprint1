93f15f9794b6cb8a2c81a05a353b89b337f3e691
ChugunovEgor-gif <mlgtop2018@gmail.com>
2024-10-05 18:57:46 +0300

    Task1

---
+++ Tyuiu.ChugunovEM.Sprint1.Task1.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.ComponentModel;
+using Tyuiu.ChugunovEM.Sprint1.Task1.V21.Lib;
+
+namespace Tyuiu.ChugunovEM.Sprint1.Task1.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(-1, res);
+        }
+    }
+}
